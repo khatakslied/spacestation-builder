ca24d3eed630fbe233a2f9d8a9b11d3aebaa9f7d
Ryan Johnson <ryan.a.y.johnson@gmail.com>
2021-12-07 02:51:41 +0900

    Add generate_attachment_spaces method to SpaceStation model.

--- app/models/space_station.rb
+++ app/models/space_station.rb
@@ -5,4 +5,13 @@ class SpaceStation < ApplicationRecord
   validates :name, presence: true
   validates :capacity, presence: true, numericality: { only_integer: true }
   validates :power, presence: true, numericality: { only_integer: true }
+  after_create :generate_attachment_spaces
+
+  def generate_attachment_spaces
+    12.times do
+      @attachment_space = AttachmentSpace.new
+      @attachment_space.space_station = self
+      @attachment_space.save!
+    end
+  end
 end
