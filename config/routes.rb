0a8d312a2daed5eb0f3b8aa47d36102c79f2f5c7
Ryan Johnson <ryan.a.y.johnson@gmail.com>
2021-12-07 04:45:41 +0900

    Add update route to attachment_spaces.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root to: 'space_stations#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :space_stations do
-    resources :attachment_spaces, only: [:new, :create]
+    resources :attachment_spaces, only: [:new, :create, :update]
   end
 end
